41a2fbeecae1cbe741ab19b452c5a127f43b3414
MBo2101 <mislaw1@gmail.com>
2021-07-13 17:14:44 -0400

    1

--- Image.py
+++ Image.py
@@ -12,69 +12,82 @@ class Image(object):
     
     def __init__(self, path):
         self.__path = path
+        self.__array_1D = None
 
+    # Properties
+    
     @property
     def path(self, path = ''):
         if path != '':
             self.__path = path
         return self.__path
-    
-    @property
-    def header(self):
-        return self.__header
-    @property
-    def data_type(self):
-        return self.__data_type
-    @property
-    def origin_x(self):
-        return self.__origin_x
-    @property
-    def origin_y(self):
-        return self.__origin_y
-    @property
-    def origin_z(self):
-        return self.__origin_z
-    @property
-    def size_x(self):
-        return self.__size_x
-    @property
-    def size_y(self):
-        return self.__size_y
-    @property
-    def size_z(self):
-        return self.__size_z
-    @property
-    def spacing_x(self):
-        return self.__spacing_x
-    @property
-    def spacing_y(self):
-        return self.__spacing_y
-    @property
-    def spacing_z(self):
-        return self.__spacing_z
-    @property
-    def array_1D(self):
-        return self.__array_1D
     @property
     def array_3D(self):
         return self.__array_3D
-        
+    @property
+    def array_1D(self):
+        if self.__array_1D is None:
+            self.__array_1D = self.array_3D_to_1D(self.__array_3D)
+        return self.__array_1D
+    @property
+    def n_voxels(self):
+        return self.__array_3D.shape[0] * self.__array_3D.shape[1] * self.__array_3D.shape[2]
+    @property
+    def data_type(self):
+        return self.__array_3D.dtype
+    @property
+    def origin_x(self):
+        return self.__header.offset[0]
+    @property
+    def origin_y(self):
+        return self.__header.offset[1]
+    @property
+    def origin_z(self):
+        return self.__header.offset[2]
+    @property
+    def size_x(self):
+        return self.__array_3D.shape[0]
+    @property
+    def size_y(self):
+        return self.__array_3D.shape[1]
+    @property
+    def size_z(self):
+        return self.__array_3D.shape[2]
+    @property
+    def spacing_x(self):
+        return self.__header.spacing[0]
+    @property
+    def spacing_y(self):
+        return self.__header.spacing[1]
+    @property
+    def spacing_z(self):
+        return self.__header.spacing[2]
+    @property
+    def direction_x(self):
+        return self.__header.direction[0]
+    @property
+    def direction_y(self):
+        return self.__header.direction[1]
+    @property
+    def direction_z(self):
+        return self.__header.direction[2]
+
+    # Methods
+    
     def load_image_medpy(self):
         self.__array_3D, self.__header = load(self.__path)
-        self.__data_type = self.__array_3D.dtype
-        self.__origin_x = self.__header.offset[0]
-        self.__origin_y = self.__header.offset[1]
-        self.__origin_z = self.__header.offset[2]
-        self.__size_x = self.__array_3D.shape[0]
-        self.__size_y = self.__array_3D.shape[1]
-        self.__size_z = self.__array_3D.shape[2]
-        self.__spacing_x = self.__header.spacing[0]
-        self.__spacing_y = self.__header.spacing[1]
-        self.__spacing_z = self.__header.spacing[2]
-        
-    def array_3D_to_1D(self):
-        array_temp = np.flip(self.__array_3D, 2)
+
+    def print_properties(self):
+        props = [p for p in dir(Image) if isinstance(getattr(Image,p), property) and hasattr(self,p)]
+        for p in props:
+            print(p + ' = ' +str(getattr(self, p)))
+    
+    # Static methods
+    
+    @staticmethod
+    def array_3D_to_1D(array_3D):
+        array_temp = np.flip(array_3D, 2)
         array_temp = np.flipud(array_temp)
         array_temp = np.rot90(array_temp, 3, (0,2))
-        self.__array_1D = np.reshape(array_temp, -1)
-        return self.__array_1D
+        array_1D = np.reshape(array_temp, -1)
+        return array_1D
