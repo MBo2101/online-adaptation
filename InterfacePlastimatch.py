1df864186fd272bb978a1c56e25219dfd77db7a4
MBo2101 <mislaw1@gmail.com>
2021-07-14 11:51:53 -0400

    2

---
+++ InterfacePlastimatch.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 14 10:28:25 2021
+
+@author: MBo
+"""
+
+from RTObject3D import *
---
+++ RTObject3D.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 13 10:30:55 2021
+
+@author: MBo
+"""
+
+import numpy as np
+from medpy.io import load
+
+'''
+Superclass
+'''
+
+class RTObject3D(object):
+    
+    def __init__(self, path):
+        self.__path = path
+        self.__array_1D = None
+
+    # Properties
+    
+    @property
+    def path(self, path = ''):
+        if path != '':
+            self.__path = path
+        return self.__path
+    @property
+    def array_3D(self):
+        return self.__array_3D
+    @property
+    def array_1D(self):
+        if self.__array_1D is None:
+            self.__array_1D = self.array_3D_to_1D(self.__array_3D)
+        return self.__array_1D
+    @property
+    def n_voxels(self):
+        return self.__array_3D.shape[0] * self.__array_3D.shape[1] * self.__array_3D.shape[2]
+    @property
+    def data_type(self):
+        return self.__array_3D.dtype
+    @property
+    def origin_x(self):
+        return self.__header.offset[0]
+    @property
+    def origin_y(self):
+        return self.__header.offset[1]
+    @property
+    def origin_z(self):
+        return self.__header.offset[2]
+    @property
+    def size_x(self):
+        return self.__array_3D.shape[0]
+    @property
+    def size_y(self):
+        return self.__array_3D.shape[1]
+    @property
+    def size_z(self):
+        return self.__array_3D.shape[2]
+    @property
+    def spacing_x(self):
+        return self.__header.spacing[0]
+    @property
+    def spacing_y(self):
+        return self.__header.spacing[1]
+    @property
+    def spacing_z(self):
+        return self.__header.spacing[2]
+    @property
+    def direction_x(self):
+        return self.__header.direction[0]
+    @property
+    def direction_y(self):
+        return self.__header.direction[1]
+    @property
+    def direction_z(self):
+        return self.__header.direction[2]
+
+    # Methods
+    
+    def load_image_medpy(self):
+        self.__array_3D, self.__header = load(self.__path)
+
+    def print_properties(self):
+        props = [p for p in dir(RTObject3D) if isinstance(getattr(RTObject3D,p), property) and hasattr(self,p)]
+        for p in props:
+            print(p + ' = ' +str(getattr(self, p)))
+    
+    # Static methods
+    
+    @staticmethod
+    def array_3D_to_1D(array_3D):
+        array_temp = np.flip(array_3D, 2)
+        array_temp = np.flipud(array_temp)
+        array_temp = np.rot90(array_temp, 3, (0,2))
+        array_1D = np.reshape(array_temp, -1)
+        return array_1D
+
+'''
+Subclasses
+'''
+
+class ImageCT(RTObject3D):
+    pass
+
+class ImageCBCT(RTObject3D):
+    pass
+
+class ImageMRI(RTObject3D):
+    pass
+
+class DoseMap(RTObject3D):
+    pass
+
+class Structure(RTObject3D):
+    pass
+
+class VectorField(RTObject3D):
+    pass
+
+
