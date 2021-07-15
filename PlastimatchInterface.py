b491492f28ade13b5c9f12c7cfbcbd0ec0e78d65
MBo2101 <mislaw1@gmail.com>
2021-07-15 14:27:13 -0400

    3

--- PlastimatchInterface.py
+++ PlastimatchInterface.py
@@ -5,4 +5,47 @@ Created on Wed Jul 14 10:28:25 2021
 @author: MBo
 """
 
-from RTObject3D import *
+import subprocess
+# from RTArray import RTArray, ImageCT, ImageCBCT, ImageMRI, DoseMap, Structure, VectorField
+from RTArray import *
+
+class PlastimatchInterface(object):
+    
+    def __init__(self):
+        pass
+    
+    # Static methods
+    
+    @staticmethod
+    def print_stats(input_file:RTArray):
+
+        '''
+        Method returns image stats as a dictionary.
+        Args:
+            RTArray
+        '''
+        if not isinstance(input_file, RTArray):
+            raise TypeError('Wrong input')
+        
+        get_stats = subprocess.Popen(['plastimatch','stats',
+                                      input_file.path,
+                                      ],stdout=subprocess.PIPE)
+        get_stats.wait()
+        
+        stats = (get_stats.stdout.read()).decode('utf-8')
+        
+        print(stats)
+        
+        return stats
+        
+        # stats = stats.strip('\n')
+        
+        # keys = stats.split(' ')[::2]
+        # values = [float(i) for i in stats.split(' ')[1::2]]
+        
+        
+        
+        # image_stats = dict(zip(keys, values))
+        
+        # return image_stats
+    
