89595a1ecc0bf23e335c9ec0e7fc5a36c43a3be9
MBo2101 <mislaw1@gmail.com>
2021-08-12 18:38:30 -0400

    18

--- SpotMap.py
+++ SpotMap.py
@@ -11,19 +11,23 @@ class SpotMap(object):
 
     def __init__(self, **kwargs):
         
-        self.__n_beamlets = len(kwargs.get('weights'))
-        self.__n_layers   = len(np.unique(kwargs.get('energies')))
+        self.__beamlet_energies    = np.array(kwargs.get('energies'),      dtype='d')
+        self.__x_coordinates       = np.array(kwargs.get('x_coordinates'), dtype='d')
+        self.__y_coordinates       = np.array(kwargs.get('y_coordinates'), dtype='d')
+        self.__beamlet_weights_MU  = np.array(kwargs.get('weights_mu'),    dtype='d')
+        self.__beamlet_weights_Gp  = np.array(kwargs.get('weights_gp'),    dtype='d')
+        self.__beam_name           = np.array(kwargs.get('beam_name'),     dtype='U')
+        
+        self.__n_beams    = len(np.unique(self.__beam_name))
+        self.__n_beamlets = len(self.__beamlet_energies)
+        self.__n_layers   = len(np.unique(self.__beamlet_energies))
         self.__beamlet_indices = np.arange(self.__n_beamlets, dtype='int64')
-        
-        self.__beamlet_energies = np.array(kwargs.get('energies'),      dtype='d')
-        self.__x_coordinates    = np.array(kwargs.get('x_coordinates'), dtype='d')
-        self.__y_coordinates    = np.array(kwargs.get('y_coordinates'), dtype='d')
-        self.__beamlet_weights  = np.array(kwargs.get('weights'),       dtype='d') # Weights in Gp
-        
-        self.__total_beam_weight = np.sum(self.__beamlet_weights)
 
     # Properties
     
+    @property
+    def n_beams(self):
+        return self.__n_beams
     @property
     def n_beamlets(self):
         return self.__n_beamlets
@@ -40,11 +44,14 @@ class SpotMap(object):
     def y_coordinates(self):
         return self.__y_coordinates
     @property
-    def weights(self):
-        return self.__beamlet_weights
+    def weights_MU(self):
+        return self.__beamlet_weights_MU
     @property
-    def total_beam_weight(self):
-        return self.__total_beam_weight
+    def weights_Gp(self):
+        return self.__beamlet_weights_Gp
+    @property
+    def beam_name(self):
+        return self.__beam_name
 
     # Methods
 
@@ -54,4 +61,36 @@ class SpotMap(object):
         for p in props:
             print(p + ' = ' +str(getattr(self, p)))
 
-
+    def get_tramp_matrix(self):
+        arr = np.array([self.__beamlet_energies,
+                        self.__x_coordinates,
+                        self.__y_coordinates,
+                        self.__beamlet_weights_Gp]).T
+        return arr
+    
+    def get_for_beam_index(self, beam_index):
+        beam = np.unique(self.__beam_name)[beam_index]
+        indices = np.where(beam == self.__beam_name)[0]
+        return SpotMap(energies = self.__beamlet_energies[indices],
+                       x_coordinates = self.__x_coordinates[indices],
+                       y_coordinates = self.__y_coordinates[indices],
+                       weights_mu = self.__beamlet_weights_MU[indices],
+                       weights_gp = self.__beamlet_weights_Gp[indices],
+                       beam_name = self.__beam_name[indices])
+    
+    def split_per_beam(self):
+        if self.__n_beams > 1:
+            lst = []
+            for beam_index in np.arange(self.__n_beams):
+                lst.append(self.get_for_beam_index(beam_index))
+            return tuple(lst)
+        elif self.__n_beams == 1:
+            return self
+    
+    def load_from_rtplan(rtplan_file_path):
+        pass
+    
+    def load_from_tramps(tramps_folder_path):
+        pass
+    
+    
