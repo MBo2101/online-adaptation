f8e93171abd2df4ee14832daceae23fa056c16e2
MBo2101 <mislaw1@gmail.com>
2021-08-13 17:47:43 -0400

    19

---
+++ BeamModel.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 14 10:28:25 2021
+
+@author: MBo
+"""
+
+import numpy as np
+import pydicom
+from scipy import interpolate
+
+class BeamModel(object):
+
+    def __init__(self, name):
+        self.__name = name
+    
+    # Properties
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    # Methods
+    
+    def print_properties(self):
+        c = self.__class__
+        props = [p for p in dir(c) if isinstance(getattr(c,p), property) and hasattr(self,p)]
+        for p in props:
+            print(p + ' = ' +str(getattr(self, p)))
+
+    def get_MU2Gp_factor(self, beamlet_energy):
+    
+        if self.__name == 'RSL_IBA_DED':
+    
+            energies_MeV = np.array([70,      75,      80,      85,      90,      95,      100,     105,     110,     115,     120,     125,     130,     135,     140,     145,     150,     155,     160,     165,     170,     175,     180,     185,     190,     195,     200,     205,     210,     215,     220,     225,     226.7])
+            ions_per_MU  = np.array([5.997E7, 6.440E7, 6.870E7, 7.250E7, 7.652E7, 8.018E7, 8.390E7, 8.676E7, 9.000E7, 9.339E7, 9.658E7, 9.937E7, 1.024E8, 1.053E8, 1.086E8, 1.115E8, 1.148E8, 1.174E8, 1.198E8, 1.226E8, 1.255E8, 1.283E8, 1.310E8, 1.332E8, 1.357E8, 1.385E8, 1.410E8, 1.432E8, 1.453E8, 1.473E8, 1.499E8, 1.516E8, 1.524E8])
+            
+            f = interpolate.interp1d(energies_MeV, ions_per_MU)
+            
+            # Additional calibration factor needed to determine Gp in GPMC vs Gp in RayStation
+            # Calibration factor alpha with: Gp_GPMC = Gp_RS * alpha = Gp_RS * (m * E (MeV) + n)
+            # Parameters provided by Patrick Wohlfahrt
+            
+            m = -0.00021028
+            n = 0.921474
+            
+            return f(beamlet_energy) * (m*beamlet_energy+n) / 10**9
+    
+    # def get_beam_name(rtplan_file_path):
+    #     ds = pydicom.dcmread(rtplan_file_path)
+    #     beam_name = ds.IonBeamSequence[0].get(['4001', '1012']).value.decode('utf-8').strip(' ')
+    #     return beam_name
+        
--- SpotMap.py
+++ SpotMap.py
@@ -6,25 +6,41 @@ Created on Wed Jul 14 10:28:25 2021
 """
 
 import numpy as np
+# from pydicom import dcmread
+import pydicom
+from BeamModel import BeamModel
 
 class SpotMap(object):
 
-    def __init__(self, **kwargs):
+    def __init__(self, rtplan_file_path=None, tramps_folder_path=None, **kwargs):
         
-        self.__beamlet_energies    = np.array(kwargs.get('energies'),      dtype='d')
-        self.__x_coordinates       = np.array(kwargs.get('x_coordinates'), dtype='d')
-        self.__y_coordinates       = np.array(kwargs.get('y_coordinates'), dtype='d')
-        self.__beamlet_weights_MU  = np.array(kwargs.get('weights_mu'),    dtype='d')
-        self.__beamlet_weights_Gp  = np.array(kwargs.get('weights_gp'),    dtype='d')
-        self.__beam_name           = np.array(kwargs.get('beam_name'),     dtype='U')
+        if rtplan_file_path is not None:
+            self.load_from_rtplan(rtplan_file_path)
+            
+        elif tramps_folder_path is not None:
+            self.load_from_tramps(tramps_folder_path)
         
-        self.__n_beams    = len(np.unique(self.__beam_name))
+        else:
+            self.__beamlet_energies    = np.array(kwargs.get('energies'),      dtype='d')
+            self.__x_coordinates       = np.array(kwargs.get('x_coordinates'), dtype='d')
+            self.__y_coordinates       = np.array(kwargs.get('y_coordinates'), dtype='d')
+            self.__beamlet_weights_MU  = np.array(kwargs.get('weights_mu'),    dtype='d')
+            self.__beamlet_weights_Gp  = np.array(kwargs.get('weights_gp'),    dtype='d')
+            self.__beamlet_labels      = np.array(kwargs.get('labels'),        dtype='U')
+        
+        u, ind = np.unique(self.__beamlet_labels, return_index=True)
+        
+        self.__beam_names = u[np.argsort(ind)]
+        self.__n_beams    = len(self.__beam_names)
         self.__n_beamlets = len(self.__beamlet_energies)
         self.__n_layers   = len(np.unique(self.__beamlet_energies))
         self.__beamlet_indices = np.arange(self.__n_beamlets, dtype='int64')
 
     # Properties
-    
+
+    @property
+    def beam_names(self):
+        return self.__beam_names
     @property
     def n_beams(self):
         return self.__n_beams
@@ -50,8 +66,8 @@ class SpotMap(object):
     def weights_Gp(self):
         return self.__beamlet_weights_Gp
     @property
-    def beam_name(self):
-        return self.__beam_name
+    def beamlet_labels(self):
+        return self.__beamlet_labels
 
     # Methods
 
@@ -61,6 +77,10 @@ class SpotMap(object):
         for p in props:
             print(p + ' = ' +str(getattr(self, p)))
 
+    def get_beam_indices(self, beam_index):
+        name = self.__beam_names[beam_index]
+        return np.where(name == self.__beamlet_labels)[0]
+
     def get_tramp_matrix(self):
         arr = np.array([self.__beamlet_energies,
                         self.__x_coordinates,
@@ -68,27 +88,74 @@ class SpotMap(object):
                         self.__beamlet_weights_Gp]).T
         return arr
     
-    def get_for_beam_index(self, beam_index):
-        beam = np.unique(self.__beam_name)[beam_index]
-        indices = np.where(beam == self.__beam_name)[0]
-        return SpotMap(energies = self.__beamlet_energies[indices],
-                       x_coordinates = self.__x_coordinates[indices],
-                       y_coordinates = self.__y_coordinates[indices],
-                       weights_mu = self.__beamlet_weights_MU[indices],
-                       weights_gp = self.__beamlet_weights_Gp[indices],
-                       beam_name = self.__beam_name[indices])
+    # def get_for_beam_index(self, beam_index):
+    #     beam = np.unique(self.__beam_names)[beam_index]
+    #     indices = np.where(beam == self.__beam_names)[0]
+    #     return SpotMap(energies = self.__beamlet_energies[indices],
+    #                    x_coordinates = self.__x_coordinates[indices],
+    #                    y_coordinates = self.__y_coordinates[indices],
+    #                    weights_mu = self.__beamlet_weights_MU[indices],
+    #                    weights_gp = self.__beamlet_weights_Gp[indices],
+    #                    beam_names = self.__beam_names[indices])
     
-    def split_per_beam(self):
-        if self.__n_beams > 1:
-            lst = []
-            for beam_index in np.arange(self.__n_beams):
-                lst.append(self.get_for_beam_index(beam_index))
-            return tuple(lst)
-        elif self.__n_beams == 1:
-            return self
+    # def split_per_beam(self):
+    #     if self.__n_beams > 1:
+    #         lst = []
+    #         for beam_index in np.arange(self.__n_beams):
+    #             lst.append(self.get_for_beam_index(beam_index))
+    #         return tuple(lst)
+    #     elif self.__n_beams == 1:
+    #         return self
     
-    def load_from_rtplan(rtplan_file_path):
-        pass
+    def load_from_rtplan(self, rtplan_file_path):
+        energies      = np.array([], dtype='d')
+        x_coordinates = np.array([], dtype='d')
+        y_coordinates = np.array([], dtype='d')
+        weights_mu    = np.array([], dtype='d')
+        labels        = np.array([], dtype='U')
+        ds = pydicom.dcmread(rtplan_file_path)
+        
+        for beam_index in range(len(ds.IonBeamSequence)):
+            beam_name = ds.IonBeamSequence[beam_index].BeamName
+            ICPS = ds.IonBeamSequence[beam_index].IonControlPointSequence
+            n_layers = int(len(ICPS)/2)
+            
+            for layer_index in range(n_layers):
+                n_spots_in_layer = int(len(ICPS[layer_index*2].ScanSpotPositionMap)/2)
+                
+                for beamlet_index in range(n_spots_in_layer):
+                    energy = ICPS[layer_index*2].NominalBeamEnergy
+                    x_coordinate = ICPS[layer_index*2].ScanSpotPositionMap[beamlet_index*2]
+                    y_coordinate = ICPS[layer_index*2].ScanSpotPositionMap[beamlet_index*2+1]
+                    
+                    if n_spots_in_layer == 1:
+                        weight_mu = ICPS[layer_index*2].ScanSpotMetersetWeights
+                    else:
+                        weight_mu = ICPS[layer_index*2].ScanSpotMetersetWeights[beamlet_index]
+                        
+                    energies      = np.append(energies,      energy)
+                    x_coordinates = np.append(x_coordinates, x_coordinate)
+                    y_coordinates = np.append(y_coordinates, y_coordinate)
+                    weights_mu    = np.append(weights_mu,    weight_mu)
+                    labels        = np.append(labels,        beam_name)
+        
+        self.__beamlet_energies    = energies
+        self.__x_coordinates       = x_coordinates
+        self.__y_coordinates       = y_coordinates
+        self.__beamlet_weights_MU  = weights_mu
+        self.__beamlet_labels      = labels
+    
+        #     total_weight_MU = total_weight_MU + spot_weight_MU
+        #     total_weight_Gp = total_weight_Gp + spot_weight_Gp
+
+        #     spot_map_string = spot_map_string + "{}\t".format(round(beam_energy,3)) + "{}\t".format(round(spot_position_x,3)) + "{}\t".format(round(spot_position_y,3)) + "{}\n".format(round(spot_weight_Gp,9))
+    
+        #     MU_per_layer += spot_weight_MU
+        #     Gp_per_layer += spot_weight_Gp
+    
+        # spots_per_layer_list.append(spots_per_layer)
+        # MU_per_layer_list.append(round(MU_per_layer,2))
+        # Gp_per_layer_list.append(round(Gp_per_layer,2))
     
     def load_from_tramps(tramps_folder_path):
         pass
